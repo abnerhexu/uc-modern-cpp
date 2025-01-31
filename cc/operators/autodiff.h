bfeab8a76ac2c2840990f1830f2ea731b27327b4
abnerhexu <hexu@rochexp.com>
2025-01-31 11:33:08 +0800

    add more case in scalar autodiff

--- cc/operators/autodiff.h
+++ cc/operators/autodiff.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <memory>
 #include <cmath>
+#include <unordered_map>
 
 namespace autodiff {
 
@@ -22,23 +23,26 @@ auto central_difference(std::vector<T>& vec, F func, std::size_t arg, float epsi
 
 class ScalarFunction {
 public:
-    std::vector<std::shared_ptr<ScalarFunction>> ctx;
     float data;
+    float grad;
+    int degree = 0;
 public:
     ScalarFunction() {}
-    ScalarFunction(float data) : data(data) {}
 }; // class ScalarFunction
 
-class Add {
+class Add: public ScalarFunction {
 public:
     std::shared_ptr<ScalarFunction> a;
     std::shared_ptr<ScalarFunction> b;
 public:
-    Add(std::shared_ptr<ScalarFunction> a, std::shared_ptr<ScalarFunction> b) : a(a), b(b) {}
-    std::shared_ptr<ScalarFunction> forward() {
-        return std::make_shared<ScalarFunction>(a->data + b->data);
+    Add(std::shared_ptr<ScalarFunction> a, std::shared_ptr<ScalarFunction> b): a(a), b(b) {
+        this->data = a->data + b->data;
+        this->degree = 2;
     }
-    std::vector<std::shared_ptr<ScalarFunction>> backward(std::shared_ptr<ScalarFunction> d_input) {
+    float forward() {
+        return a->data + b->data;
+    }
+    std::vector<float> backward(float d_input) {
         return {d_input, d_input};
     }
 }; // class Add
@@ -47,15 +51,69 @@ class Log: public ScalarFunction {
 public:
     std::shared_ptr<ScalarFunction> a;
 public:
-    Log(std::shared_ptr<ScalarFunction> a): a(a) {}
-    std::shared_ptr<ScalarFunction> forward() {
-        this->ctx.push_back(a);
-        return std::make_shared<ScalarFunction>(logf(a->data));
+    Log(std::shared_ptr<ScalarFunction> a): a(a) {
+        this->data = this->forward();
+        this->degree = 1;
     }
-    std::vector<std::shared_ptr<ScalarFunction>> backward(std::shared_ptr<ScalarFunction> d_input) {
-        return {std::make_shared<ScalarFunction>(1.0f * d_input->data / this->ctx[0]->data)};
+    float forward() {
+        return logf(a->data);
+    }
+    std::vector<float> backward(float d_input) {
+        return {(1.0f * d_input / a->data)};
     }
 }; // class Log
 
+class Mul: public ScalarFunction {
+public:
+    std::shared_ptr<ScalarFunction> a;
+    std::shared_ptr<ScalarFunction> b;
+public:
+    Mul(std::shared_ptr<ScalarFunction> a, std::shared_ptr<ScalarFunction> b) : a(a), b(b) {
+        this->data = this->forward();
+        this->degree = 2;
+    }
+    float forward() {
+        return a->data * b->data;
+    }
+    std::vector<float> backward(float d_input) {
+        return {d_input * b->data, d_input * a->data};
+    }
+}; // class Mul
 
+class Inv: public ScalarFunction {
+public:
+    std::shared_ptr<ScalarFunction> a;
+public:
+    Inv(std::shared_ptr<ScalarFunction> a): a(a) {
+        this->data = this->forward();
+        this->degree = 1;
+    }
+    float forward() {
+        return 1.0f / a->data;
+    }
+    std::vector<float> backward(float d_input) {
+        return {-1.0f * d_input / (a->data * a->data)};
+    }
+}; // class Inv
+
+class Sigmoid: public ScalarFunction {
+public:
+    std::shared_ptr<ScalarFunction> a;
+public:
+    Sigmoid(std::shared_ptr<ScalarFunction> a): a(a) {
+        this->data = this->forward();
+        this->degree = 1;
+    }
+    float forward() {
+        if (this->a->data >= 0.0) {
+            return 1.0 / (1.0 + expf(-this->a->data));
+        }
+        else {
+            return expf(this->a->data) / (1.0 + expf(this->a->data));
+        }
+    }
+    std::vector<float> backward(float d_input) {
+        return {d_input * this->data * (1.0f - this->data)};
+    }
+}; // class Sigmoid
 }
